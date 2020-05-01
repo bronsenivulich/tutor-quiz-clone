3ae711f43d4ee3bf648f324b5a843518055c86bb
Lauren Campbell <laurencampbell@westnet.com.au>
2020-05-01 17:41:10 +0800

    rename models

--- app/models.py
+++ app/models.py
@@ -9,6 +9,11 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+class UserRelationship(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tutorId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    studentId = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(64), index=True, unique=True)
@@ -17,8 +22,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     passwordHash = db.Column(db.String(128))
     userType = db.Column(db.Integer, db.ForeignKey('user_type.id'))
-    students = db.relationship("User",secondary="job",primaryjoin=id==Job.tutorId,secondaryjoin=id==Job.studentId,backref="tutor")
-    tutors = db.relationship("User",secondary="job",primaryjoin=id==Job.studentId,secondaryjoin=id==Job.tutorId,backref="student")
+    students = db.relationship("User",secondary="user_relationship",primaryjoin=id==UserRelationship.tutorId,secondaryjoin=id==UserRelationship.studentId,backref="tutor")
+    tutors = db.relationship("User",secondary="user_relationship",primaryjoin=id==UserRelationship.studentId,secondaryjoin=id==UserRelationship.tutorId,backref="student")
     quizzes = db.relationship('Quiz', backref='creator', lazy='dynamic')
 
     def __repr__(self):
@@ -34,11 +39,6 @@ class User(UserMixin, db.Model):
     def set_userType(self, userType):
         self.userType =  UserType.query.filter_by(userType=userType).first().id
 
-
-class UserRelationship(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tutorId = db.Column(db.Integer, db.ForeignKey('user.id'))
-    studentId = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class UserType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
