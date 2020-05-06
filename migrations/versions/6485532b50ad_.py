894d82a1882178fff50fa537946a1c8c8bde6bc3
Lauren Campbell <laurencampbell@westnet.com.au>
2020-05-06 20:36:52 +0800

    fix multiple head error

---
+++ migrations/versions/6485532b50ad_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6485532b50ad
+Revises: c5fc85247993, de797f4b64b0
+Create Date: 2020-05-06 20:35:20.009355
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6485532b50ad'
+down_revision = ('c5fc85247993', 'de797f4b64b0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
