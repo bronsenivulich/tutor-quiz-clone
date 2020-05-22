3580a1f13b5f5d9b7ca876aa5a31c1353cf8d48c
Benjamin Podmore <benjaminpodmore@gmail.com>
2020-05-23 00:06:58 +0800

    Add api and html code for students to complete quizzes and get a score

---
+++ app/static/js/complete-quiz.js
@@ -0,0 +1,108 @@
+$(document).ready(() => {
+
+    let quizId = $("#data").data("quiz-id")
+    let token = $("#data").data("token")
+
+    function showQuiz(quizData) {
+        let questions = quizData.questions
+        let qNum = 1
+
+        questions.forEach(function (entry) {
+            if (entry.questionType == "shortAnswer") {
+                $('#completeQuiz').append(`
+                <div id="quizQuestion_${qNum}" class="wholeQuestion shortAnswer">
+                <h4>Question: ${qNum}</h4>
+                <p class="pb-1">
+                    <span class="question" id="questionId_${entry.questionId}">${entry.question}</span><br>
+                    <label class="form-headers mt-2 mr-2">Answer</label>
+                    <input type='text' class="form-fields answer"></input>
+                </p>
+                </div>`);
+            }
+            else if (entry.questionType == "multiSolution") {
+                $('#completeQuiz').append(`
+                <div id="question_${qNum}" class="wholeQuestion multiSolution">
+                <h4>Question: ${qNum}</h4>
+                <p>
+                <span class="question" id="questionId_${entry.questionId}">${entry.question}</span><br>
+                <div class="row d-flex justify-content-center px-5 mx-5"></div>
+                </p>
+                </div>
+                `);
+                let optionNum = 1;
+                entry.options.forEach(function (option) {
+                    $(`#question_${qNum}`).find(".row").append(`<div class="px-3"><label class="mr-2" for="choice_${option.choiceId}">${option.answer}</label><input type="radio" id="choice_${option.choiceId}" name="question_${entry.questionId}" value="${option.choiceId}"></div>
+                    `);
+                    optionNum = optionNum + 1;
+                })
+            }
+            qNum = qNum + 1;
+        });
+    }
+
+
+
+
+    $.ajax({
+        url: `/api/quizzes/${quizId}`,
+        type: "get",
+        contentType: "application/json",
+        headers: { "Authorization": 'Bearer ' + token },
+        success: function (data) {
+            showQuiz(data)
+        },
+        error: function (resp) {
+        }
+    });
+
+    $("#submitQuiz").click(function () {
+        console.log("click")
+        $("#completeQuiz").submit();
+    });
+
+    $("#completeQuiz").submit(function () {
+
+        let answers = $("#completeQuiz").children(".wholeQuestion").toArray()
+
+        let allAnswers = []
+
+        answers.forEach(function (entry) {
+            if ($(entry).hasClass("shortAnswer")){
+                answer = {
+                    "questionType": "shortAnswer",
+                    "questionId": $(entry).find("span").attr("id").replace("questionId_",""),
+                    "studentAnswer": $(entry).find(".answer").val()
+                }
+                allAnswers.push(answer)
+            }
+            else if( $(entry).hasClass("multiSolution")){
+                answer = {
+                    "questionType": "multiSolution",
+                    "questionId": $(entry).find("span").attr("id").replace("questionId_",""),
+                    "studentAnswer": $(entry).find("input:checked").val()
+                }
+                allAnswers.push(answer)
+            }
+        });
+
+        data = {
+            "answers":allAnswers
+        }
+
+        $.ajax({
+            url: `/api/quizzes/submit/${quizId}`,
+            data: JSON.stringify(data),
+            type: "post",
+            contentType: "application/json",
+            headers: { "Authorization": 'Bearer ' + token },
+            success: function (data) {
+                // console.log(data)
+            },
+            error: function (resp) {
+                // console.log(resp)
+            }
+        });
+
+    });
+
+});
