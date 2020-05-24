800b8a22d090b491c036c04a543bde84e2a5ea66
Benjamin Podmore <benjaminpodmore@gmail.com>
2020-05-25 00:21:48 +0800

    Remove forgotten password and email features

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,10 @@ from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app, db
 
-from app.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm, RequestStudentForm, AssignStudentForm
+from app.forms import LoginForm, RegistrationForm, RequestStudentForm, AssignStudentForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, UserType, Request, Quiz, Question, ShortAnswer, StudentQuiz, UserRelationship, Score
 
-from app.email import send_password_reset_email
 import os
 from flask import send_from_directory
 
@@ -120,45 +119,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-# Request forgotten password page
-@app.route('/reset-password-request', methods=['GET', 'POST'])
-def reset_password_request():
-
-    # Check user authentication
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    # If user is valid, send email to user
-    form = ResetPasswordRequestForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password')
-        return redirect(url_for('login'))
-    return render_template('reset-password-request.html', 
-                            title='Reset Password', form=form)
-
-
-# Reset password page
-@app.route('/reset-password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-
-    # Check user authentication
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('index'))
-
-    # Update password on database
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash('Your password has been reset.')
-        return redirect(url_for('login'))
-    return render_template('reset-password.html', form=form)
 
 # Request student page
 @app.route('/request-student', methods=['GET', 'POST'])
