83c8b781533c84d3f278ac3ef5236f0d66bb94a1
Benjamin Podmore <benjaminpodmore@gmail.com>
2020-05-25 15:39:15 +0800

    Add basic quiz testing

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Quiz, UserRelationship, StudentQuiz, Question, ShortAnswer, MultiSolution
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -14,17 +14,62 @@ class TestCase(unittest.TestCase):
         self.app = app.test_client()
         print(app.config['SQLALCHEMY_DATABASE_URI'])
         db.create_all()
+        testTutor = User(id=1,username="test_tutor", userType=1)
+        testStudent = User(id=2, username="test_student", userType=2)
+        userRelationship = UserRelationship(tutorId=1, studentId=2)
+        quiz = Quiz(name="Test Quiz", body="Test Body",tutorId=1)
+        db.session.add(testTutor)
+        db.session.add(testStudent)
+        db.session.add(userRelationship)
+        db.session.add(quiz)
+        db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
-    def test_unique_username(self):
-        u = User(username="foo", email="user@example.com")
-        db.session.add(u)
+    def test_quiz_tutor_id(self):
+
+        quiz = Quiz.query.filter_by(name="Test Quiz").first()
+        tutor = User.query.filter_by(username="test_tutor").first()
+
+        assert quiz.tutorId == tutor.id
+
+    def test_quiz_student_access(self):
+        quiz = Quiz.query.filter_by(name="Test Quiz").first()
+        student = User.query.filter_by(username="test_student").first()
+        studentQuiz = StudentQuiz(quizId=1, studentId=2)
+        db.session.add(studentQuiz)
         db.session.commit()
-        username = "bar"
-        assert username != User.query.filter_by(email="user@example.com").first().username
+
+        assert student.id == User.query.filter_by(id=StudentQuiz.query.filter_by(quizId=1,studentId=2).first().studentId).first().id
+
+
+    def test_short_answer_questio(self):
+        question = Question(id=1, quizId=1,question="What is 1+1?")
+        shortAnswer = ShortAnswer(questionId=1, correctAnswer="2")
+        db.session.add(question)
+        db.session.add(shortAnswer)
+        db.session.commit()
+
+        assert "2" == shortAnswer.query.filter_by(questionId=question.id).first().correctAnswer
+
+
+    def test_multi_choice_question(self):
+        question = Question(id=1, quizId=1,question="What is 1+1?")
+        multiChoiceA = MultiSolution(questionId=1, possibleAnswer="2", correctAnswer=True)
+        multiChoiceB = MultiSolution(questionId=1, possibleAnswer="100", correctAnswer=False)
+        db.session.add(question)
+        db.session.add(multiChoiceA)
+        db.session.add(multiChoiceB)
+        db.session.commit()
+
+        correctAnswer = multiChoiceA.id
+        incorrectAnswer = multiChoiceB.id
+
+        assert (MultiSolution.query.filter_by(questionId=question.id,correctAnswer=True).first().id == correctAnswer) and \
+            MultiSolution.query.filter_by(questionId=question.id,correctAnswer=False).first().id == incorrectAnswer
+
 
 
 if __name__ == '__main__':
